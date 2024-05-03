e18df9b776be8506f3287e39969ac22ccb017628
rahul <rahuldharmkar@gmail.com>
2024-05-03 19:03:08 +0530

    Initial commit

---
+++ app/src/main/java/com/rahuldharmkar/qrcodescanner/MainActivity.java
@@ -0,0 +1,30 @@
+package com.rahuldharmkar.qrcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button scanbtn;
+    public static TextView scantext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scantext=(TextView)findViewById(R.id.scantext);
+        scanbtn=(Button) findViewById(R.id.scanbtn);
+
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ScannerActivity.class));
+            }
+        });
+    }
+}
